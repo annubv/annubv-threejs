cba6e68dc9f163096005a80833bba439ab407635
Anubhav Singh <anubhav.singh@fashinza.com>
2024-03-21 03:23:34 +0530

    Initialise project

---
+++ src/components/ThreeCanvas.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React, { useRef } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Mesh } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+function MeshComponent() {
+  const fileUrl = '/scene.gltf';
+  const mesh = useRef<Mesh>(null!);
+  const gltf = useLoader(GLTFLoader, fileUrl);
+
+  useFrame(() => {
+    // mesh.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  );
+}
+
+const ThreeCanvas = () => {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <ambientLight />
+      {/* <pointLight position={[10, 10, 10]} /> */}
+      <MeshComponent />
+    </Canvas>
+  );
+};
+
+export default ThreeCanvas;
